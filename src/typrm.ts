4a70e8ad052d30184f93830e981653585ef30107
Takakiri <takakiriy@gmail.com>
2021-07-11 00:59:53 +0900

    update version number

--- src/typrm.ts
+++ src/typrm.ts
@@ -7,7 +7,7 @@ function  exitFromCommander(e: commander.CommanderError) {
     }
 }
 async function  callMain() {
-    commander.program.version('0.5.1')  // Do not forget to transpile, update package.json, add git tag
+    commander.program.version('0.5.2')  // Do not forget to transpile, update package.json, commit and add git tag
         .exitOverride(exitFromCommander)
         .option("-l, --locale <s>")
         .option("-t, --test")
